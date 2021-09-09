c27291ecc878d174a2e7b5486e886d7582a2af96
oleksandr-suslov <Oleksandr.Suslov@gmail.com>
2021-09-09 09:26:54 +0300

    day 1

---
+++ script/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btnOpenModal = document.querySelector("#btnOpenModal");
+  const modalBlock = document.querySelector("#modalBlock");
+  const closeModal = document.querySelector("#closeModal");
+  const questionTitle = document.querySelector("#question");
+  const formAnswers = document.querySelector("#formAnswers");
+  btnOpenModal.addEventListener("click", () => {
+    modalBlock.classList.add("d-block");
+    playTest();
+  });
+  closeModal.addEventListener("click", () => {
+    modalBlock.classList.remove("d-block");
+  });
+  const playTest = () => {
+    const renderQuestions = () => {
+      questionTitle.textContent = "Какого цвета бургер вы хотите?";
+      formAnswers.innerHTML = `
+        <div class="answers-item d-flex flex-column">
+                <input type="radio" id="answerItem1" name="answer" class="d-none">
+                <label for="answerItem1" class="d-flex flex-column justify-content-between">
+                <img class="answerImg" src="./image/burger.png" alt="burger">
+                <span>Стандарт</span>
+                </label>
+              </div>
+              
+        `;
+    };
+    renderQuestions();
+  };
+});
